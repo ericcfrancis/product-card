ef2c20789e11daf6d42a9d90ed8b7ce5ed292a2e
ericcfrancis <ericcire49@gmail.com>
2025-05-02 12:11:18 +0800

    add animation

---
+++ script.js
@@ -0,0 +1,27 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    document.querySelectorAll('.add-to-cart').forEach(btn => {
+        btn.addEventListener('click', () => {
+          const icon = btn.querySelector('i');
+          icon.classList.add('cart-animate');
+          setTimeout(() => icon.classList.remove('cart-animate'), 300);
+        });
+      });
+    
+      
+      document.querySelectorAll('.like-btn').forEach(btn => {
+        btn.addEventListener('click', () => {
+          btn.classList.toggle('liked');
+          const icon = btn.querySelector('i');
+    
+          if (btn.classList.contains('liked')) {
+            icon.classList.remove('fa-regular');
+            icon.classList.add('fa-solid');
+          } else {
+            icon.classList.remove('fa-solid');
+            icon.classList.add('fa-regular');
+          }
+        });
+      });
+});
